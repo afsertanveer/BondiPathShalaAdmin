7c93062bda7040817b33b2a58b9f040869623236
Md. Afser <uiu.afser@gmail.com>
2023-10-05 10:47:43 +0600

    written exam perfectly done with multiple image check in  one question

--- src/pages/protected/SingleStudentSpecialWritten.js
+++ src/pages/protected/SingleStudentSpecialWritten.js
@@ -3,36 +3,50 @@ import { useParams } from "react-router-dom";
 import axios from "../../utils/axios";
 import { LoaderIcon, toast } from "react-hot-toast";
 import "tui-color-picker/dist/tui-color-picker.css";
-import 'tui-image-editor/dist/tui-image-editor.css';
-import ImageEditor from '@toast-ui/react-image-editor';
+import "tui-image-editor/dist/tui-image-editor.css";
+import ImageEditor from "@toast-ui/react-image-editor";
 import { whiteTheme } from "../../utils/globalVariables";
 
-const SingleStudentSpecialWritten = () => {
+const SingleStudentSpecialWritten  = () => {
   const params = useParams();
   const [singleResult, setSingleResult] = useState({});
   const [isLoading, setIsLoading] = useState(false);
   const [source, setSource] = useState([]);
   const [disabler, setDisabler] = useState([]);
-  const [buttonDisabler,setButtonDisabler] = useState(true);
-  const [finalbuttonDisabler,setFinalButtonDisabler] = useState(false);
-  const [counter,setCounter] = useState(1);
-  console.log(useParams.studentId);
+  const [buttonDisabler, setButtonDisabler] = useState(true);
+  const [finalbuttonDisabler, setFinalButtonDisabler] = useState(false);
+  const [ansTracker, setAnsTracker] = useState([]);
+  const [counter, setCounter] = useState(1);
   let prevSource = [];
   let changer = [];
   const imageEditor = React.createRef();
-  const logImageContent = ()  =>{
+  const logImageContent = () => {
     const imageEditorInst = imageEditor.current.imageEditorInst;
     const data = imageEditorInst.toDataURL();
     prevSource = [...source];
     prevSource.push(data);
     setSource(prevSource);
-    toast.success("Image Saved");  
-   }
+    toast.success("Image Saved");
+  };
+  const checkNext = (i,j)=>{
+    const imageEditorInst = imageEditor.current.imageEditorInst;
+    const data = imageEditorInst.toDataURL();
+    prevSource = [...source];
+    prevSource.push(data);
+    setSource(prevSource);
+    toast.success("Image Saved");
+    let prevTracker = ansTracker;
+    prevTracker[i][j]=0;
+    prevTracker[i][j+1]=1;
+    console.log(prevTracker,"Checking");
+    setAnsTracker(prevTracker)
+  }
   const sendImage = async (e) => {
+    
     e.preventDefault();
-    setCounter(prev=>prev+1);
-    if(counter===singleResult.totalQuestions){
-      setFinalButtonDisabler(true)
+    setCounter((prev) => prev + 1);
+    if (counter === singleResult.totalQuestions) {
+      setFinalButtonDisabler(true);
     }
     const form = e.target;
     const idx = parseInt(form.index.value);
@@ -46,16 +60,16 @@ const SingleStudentSpecialWritten = () => {
       }
     }
     let answer;
-    if(source.length===0){
-       answer = {
+    if (source.length === 0) {
+      answer = {
         questionNo: idx + 1,
         obtainedMarks,
         studentId: params.studentId,
         examId: params.examId,
         uploadImages: [],
       };
-    }else{
-       answer = {
+    } else {
+      answer = {
         questionNo: idx + 1,
         obtainedMarks,
         studentId: params.studentId,
@@ -69,7 +83,6 @@ const SingleStudentSpecialWritten = () => {
       setButtonDisabler(true);
       setSource([]);
     });
-    console.log(answer);
     setDisabler(changer);
   };
   const finalSave = async () => {
@@ -84,13 +97,18 @@ const SingleStudentSpecialWritten = () => {
         toast.success("successfully updated the result");
       });
   };
-  const checkNumber = (marks,id)=>{
-    if(isNaN(marks)===false && (parseFloat(marks))<=singleResult.marksPerQuestion[id]){
-      setButtonDisabler(false)
-    }else{
+  const checkNumber = (marks, id) => {
+    console.log(id);
+    if (
+      isNaN(marks) === false &&
+      parseFloat(marks) <= singleResult.marksPerQuestion[id]
+    ) {
+      setButtonDisabler(false);
+    } else {
+      console.log("jhere");
       setButtonDisabler(true);
     }
-  }
+  };
   useEffect(() => {
     setIsLoading(true);
     axios
@@ -108,110 +126,154 @@ const SingleStudentSpecialWritten = () => {
             dis[i] = 0;
           }
         }
+
         setDisabler(dis);
+        let tracker = [];
+        for (let i = 0; i < data.answerScript.length; i++) {
+          tracker[i] = [];
+          if (data.answerScript[i] === null) {
+            tracker[i] = [1];
+          } else {
+            if (data.answerScript[i].length > 1) {
+              for (let j = 0; j < data?.answerScript[i]?.length; j++) {
+                if (j === 0) {
+                  tracker[i][j] = 1;
+                } else {
+                  tracker[i][j] = 0;
+                }
+              }
+            } else {
+              tracker[i] = [1];
+            }
+          }
+        }
+        console.log(tracker);
+        setAnsTracker(tracker);
         setIsLoading(false);
-        // setAnswerScript(data.answerScript)
       });
   }, [params]);
   return isLoading ? (
     <LoaderIcon></LoaderIcon>
   ) : (
     <div>
-      {typeof singleResult.answerScript !== "undefined" &&
-        singleResult.answerScript.length > 0 && 
-        singleResult.answerScript.map((ans, idx) => {
-          return (
-            <div key={idx} className="my-10">
-              {disabler[idx] === 1 && (
-                <>
-                  <p>{idx + 1}</p>
-                  <div className="grid grid-cols-1">
-                    {typeof ans !== "undefined" &&
-                      ans!==null &&
-                      ans.length > 0 &&
-                      ans.map((photo, index) => {
-                        return (
-                          <Fragment
-                          key={index}
-                          >
-                            { <ImageEditor
-                              includeUI={{
-                                loadImage: {
-                                  path:process.env.REACT_APP_API_HOST +'/' +photo,
-                                  name: "SampleImage"
-                                },
-                                menu: ['draw'],
-                                initMenu: 'draw',
-                                theme:whiteTheme,
-                                uiSize: {
-                                  width: '100%',
-                                  height: '800px',
-                                },
-                                menuBarPosition: 'top',
-                              }}
-                              cssMaxHeight={700}
-                              cssMaxWidth={1500}
-                              selectionStyle={{
-                                cornerSize: 20,
-                                rotatingPointOffset: 70,
-                              }}
-                              usageStatistics={true}
-                              ref={imageEditor}
-                              />}
-                              <button className="btn mt-4 justify-center" onClick={logImageContent}>Save Image</button>
-                          </Fragment>
-                        );
-                      })}
-                      {
-                        typeof ans !== "undefined" &&
-                      ans===null && <p className="text-red-500 font-bold text-center mt-5">No answer for this question</p>
-                      }
-                      
-                      <form onSubmit={sendImage} className="mt-4 ">
-                      <input
-                        type="text"
-                        className="input input-bordered  border-black hidden"
-                        name="index"
-                        id=""
-                        defaultValue={idx}
-                      />
-                      <p className="ml-4 text-lg font-bold text-red">Marks out of {singleResult.marksPerQuestion[idx]}</p>
-                      <div className="flex ">
-                      <input
-                        type="text"
-                        name="obtMarks"
-                        id="obtMarks"
-                        autoComplete="off"
-                        className="input input-bordered  border-black"
-                        onChange={e=>checkNumber(e.target.value,idx)}
-                        required
-                      />
-                      <input
-                        type="submit"
-                        className="ml-4 btn "
-                        value="Save Marks"
-                        disabled={buttonDisabler}
-                      />
-                      </div>
-                    </form>
-                  </div>
-                  
-                </>
-              )}
-            </div>
-          );
-        })
-      }
-        
-      <div className="flex justify-center items-center">
       {
-        finalbuttonDisabler && <button className="btn" onClick={() => finalSave()}>
-        Update and save 
-      </button>
+        typeof singleResult.answerScript !== "undefined" &&
+          singleResult.answerScript.length > 0 &&
+          singleResult.answerScript.map((ans, idx) => {
+            return (
+              <div key={idx} className="my-1">
+                {disabler[idx] === 1 && (
+                  <>
+                    <p>{idx + 1}</p>
+                    <div className="grid grid-cols-1">
+                      {typeof ans !== "undefined" &&
+                        ans !== null &&
+                        ans.length > 0 &&
+                        ans.map((photo, index) => {
+                          return (
+                            <Fragment key={index}>
+                              {
+                                ansTracker[idx][index]===1 &&<Fragment>
+                                <ImageEditor
+                                includeUI={{
+                                  loadImage: {
+                                    path:
+                                      process.env.REACT_APP_API_HOST +
+                                      "/" +
+                                      photo,
+                                    name: "SampleImage",
+                                  },
+                                  menu: ["draw"],
+                                  initMenu: "draw",
+                                  theme: whiteTheme,
+                                  uiSize: {
+                                    width: "100%",
+                                    height: "800px",
+                                  },
+                                  menuBarPosition: "top",
+                                }}
+                                cssMaxHeight={700}
+                                cssMaxWidth={1500}
+                                selectionStyle={{
+                                  cornerSize: 20,
+                                  rotatingPointOffset: 70,
+                                }}
+                                usageStatistics={true}
+                                ref={imageEditor}
+                              />
+                              {
+                                ansTracker[idx][ans.length-1]===1? <button
+                                className="btn mt-4 justify-center"
+                                onClick={logImageContent}
+                              >
+                                Save Image
+                              </button> : <button
+                              className="btn mt-4 justify-center"
+                              onClick={()=>checkNext(idx,index)}
+                            >
+                              Next Image
+                            </button>
+                              }
+                                
+                                </Fragment>
+                              }
+                            </Fragment>
+                          );
+                        })}
+                      {typeof ans !== "undefined" && ans === null && (
+                        <p className="text-red-500 font-bold text-center mt-5">
+                          No answer for this question
+                        </p>
+                      )}
+
+                      <form onSubmit={sendImage} className="mt-4 ">
+                        <input
+                          type="text"
+                          className="input input-bordered  border-black hidden"
+                          name="index"
+                          id=""
+                          defaultValue={idx}
+                        />
+                        <p className="ml-4 text-lg font-bold text-red">
+                          Marks out of {singleResult.marksPerQuestion[idx]}
+                        </p>
+                        <div className="flex ">
+                          <input
+                            type="text"
+                            name="obtMarks"
+                            id="obtMarks"
+                            autoComplete="off"
+                            className="input input-bordered  border-black"
+                            onChange={(e) => checkNumber(e.target.value, idx)}
+                            required
+                          />
+                          <input
+                            type="submit"
+                            className="ml-4 btn "
+                            value="Save Marks"
+                            disabled={buttonDisabler}
+                          />
+                        </div>
+                      </form>
+                    </div>
+                  </>
+                )}
+              </div>
+            );
+          })
+
+        
       }
+
+      <div className="flex justify-center items-center">
+        {finalbuttonDisabler && (
+          <button className="btn" onClick={() => finalSave()}>
+            Update and save
+          </button>
+        )}
       </div>
     </div>
   );
 };
 
-export default SingleStudentSpecialWritten;
+export default SingleStudentSpecialWritten ;

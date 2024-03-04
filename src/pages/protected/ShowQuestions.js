d5adeb0920192bee3ede353070f8eeeead7c44b6
unknown <uiu.afser@gmail.com>
2024-03-04 08:01:40 +0600

    special changes added

--- src/pages/protected/ShowQuestions.js
+++ src/pages/protected/ShowQuestions.js
@@ -8,6 +8,7 @@ import DeactivateButton from '../../features/common/components/DeactivateButton'
 import PopUpModal from '../../features/common/components/PopUpModal'
 import { optionName } from '../../utils/globalVariables'
 import QuestionSender from '../../components/QuestionSender/QuestionSender'
+import SpecialQuestionSender from './SpecialQuestionSender'
 const ShowQuestions = () => {
   const [courses, setCourses] = useState([])
   const [subjects, setSubjects] = useState([])
@@ -562,79 +563,21 @@ const ShowQuestions = () => {
         </div>
       )}
 
-      <input type="checkbox" id="my-modal-special" className="modal-toggle" />
-      <div className="modal">
-        <div className="modal-box">
-          <form
-            onSubmit={sendQuestionSpecial}
-            className="mt-4 w-full  mx-auto flex flex-col "
-          >
-            <div className="form-control">
-              <label className="label-text text-center" htmlFor="">
-                Select Course
-              </label>
-              <select
-                className="input border-black input-bordered w-full"
-                required
-                onChange={(e) => handleChangeSecondCourse(e)}
-              >
-                <option value=""></option>
-                {courses.length > 0 &&
-                  courses.map((course) => (
-                    <option key={course._id} value={course._id}>
-                      {course.name}
-                    </option>
-                  ))}
-              </select>
-            </div>
-            <div className="form-control mr-3">
-              <label className="label-text text-center" htmlFor="">
-                Select Exam Name
-              </label>
-              <select
-                className="input  border-black input-bordered w-full"
-                required
-                onChange={(e) => setQuestionExam(e.target.value)}
-              >
-                <option value=""></option>
-                {specialExams.length > 0 &&
-                  specialExams.map((exam) => (
-                    <option key={exam._id} value={exam._id}>
-                      {exam.name}
-                    </option>
-                  ))}
-              </select>
-            </div>
-            <div className="form-control mr-3">
-              <label className="label-text text-center" htmlFor="">
-                Select Subject
-              </label>
-              <select
-                className="input  border-black input-bordered w-full"
-                required
-                onChange={(e) => setQuestionSubject(e.target.value)}
-              >
-                <option value=""></option>
-                {secondsubjects.length > 0 &&
-                  secondsubjects.map((subject) => (
-                    <option key={subject._id} value={subject._id}>
-                      {subject.name}
-                    </option>
-                  ))}
-              </select>
-            </div>
-
-            <div className="form-control mt-4  flext justify-center items-center">
-              <input type="submit" value="Add Questions" className="btn" />
-            </div>
-          </form>
-          <div className="modal-action">
-            <label htmlFor="my-modal-special" className="btn bg-red w-[80px]">
-              Close!
-            </label>
-          </div>
-        </div>
-      </div>
+      <SpecialQuestionSender 
+      sendQuestionSpecial={sendQuestionSpecial}
+      handleChangeSecondCourse={handleChangeSecondCourse}
+      courses={courses}
+      secondsubjects={secondsubjects}
+      handleChangeBothStatus={handleChangeBothStatus}
+      handleSecondExam={handleSecondExam}
+      secondexams={secondexams}
+      singleSecondExam={singleSecondExam}
+      questionExam={questionExam}
+      setQuestionExam={setQuestionExam}
+      specialExams={specialExams}
+      setQuestionSubject={setQuestionSubject}
+      setSecondSet={setSecondSet}
+      />
       <QuestionSender
         sendQuestions={sendQuestions}
         handleChangeSecondCourse={handleChangeSecondCourse}
